817707da8378139b11c09cd89ea363e237765711
@mar <30743654+shuklaamar@users.noreply.github.com>
2019-04-10 15:11:09 +0530

    Update projectOS.c

--- projectOS.c
+++ projectOS.c
@@ -5,43 +5,87 @@ struct process{
 };
 int main(){
 	int n;
-	cout<<"enter the no of processes";
+	cout<<"Enter the no of processes"<<":";
 	cin>>n;
+	if(n<0){
+		cout<<""<<endl<<"**************************************"<<endl<<""<<endl;
+		cout<<"Negative No of processes is not possible"<<endl;
+		cout<<""<<endl<<"**************************************"<<endl;
+		_exit(0);
+		
+	}
 	struct process pro[n];
 	int arr[n],bur[n],pri[n],prn[n];
 	for(int k=1;k<=n;k++){
-	cout<<"enter the arrival time of the process";
+	cout<<"Enter the arrival time of the process"<<"P"<<k<<":";
 	cin>>arr[k];
+	if(arr[k]>=0){
 	pro[k].at=arr[k];
+	}
+	else{
+		cout<<""<<endl<<"**************************************"<<endl<<""<<endl;
+		cout<<"Negative arrival is not possible"<<endl;
+		cout<<""<<endl<<"**************************************"<<endl;
+		_exit(0);
+	}
 }   
-	for(int k=1;k<=n;k++){
-	cout<<"enter the burst time of the process";
+for(int k=1;k<=n;k++){
+	cout<<"Enter the burst time of the process"<<"P"<<k<<":";
 	cin>>bur[k];
-	pro[k].bt=bur[k];
+	if(bur[k]>=0){
 	
-}   
+		pro[k].bt=bur[k];
+	}
+	else{
+		cout<<""<<endl<<"**************************************"<<endl<<" "<<endl;
+		cout<<"Negative burst is not possible"<<endl;
+		cout<<""<<endl<<"**************************************"<<endl;
+		_exit(0);
+	}
+	}   
+for(int k=1;k<=n;k++){
+		pro[k].pno=k;
+	}
     int sum=0,sor;
     for(int k=1;k<=n;k++){
     sum+=pro[k].bt;
     }
     cout<<"gantt chart"<<endl;
-    cout<<"processes are in this order"<<"P:";
+    cout<<"processes are in this order"<<"P:"<<endl;
 	for(int k=1;k<sum;k++){
 		for(int i=1;i<=n;i++){
 			if(pro[i].at<k){
 				pro[i].wt=k-pro[i].at;
 			    pro[i].pr=1+(pro[i].wt/pro[i].bt);
 			}
-		for(int i=1;i<=n;i++){
-			if(pro[i].pr>pro[i+1].pr){
-				sor=pro[i].pr;
-				pro[i].pr=pro[i+1].pr;
-				}
-				}
-			
-		}
+    }
+    int max=pro[1].pr;
+    for(int i=1;i<=n;i++){
+    	if(pro[i].pr<max){
+    		max=pro[i].pr;
+    		cout<<"P:"<<i<<endl;	
+		}	
+		
 	}
-	for(int i=1;i<=n;i++){
 	
 	}
+	int avg_wt;
+	for(int i=1;i<=n;i++){
+		
+	avg_wt=pro[i].wt+pro[i+1].wt;
+	}
+	if(avg_wt>0){
+		cout<<endl<<"Average waiting time of the processes is:"<<avg_wt/n<<endl;
+	}
+	else{
+	cout<<"Average waiting time of the processes is:"<<-avg_wt/n<<endl;
+}
+    int avg_tat;
+    if(avg_wt>0){
+		cout<<"Average turn arround time of the processes is:"<<avg_wt+sum/n<<endl;
+	}
+	else{
+	cout<<"Average turn arround time of the processes is:"<<-avg_wt+sum/n<<endl;
+}
+    
 }
